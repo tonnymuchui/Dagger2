d0190e16ab0481651ecd01beda0bbc0ee35f5529
tonnymuchui <tonykanyingah@gmail.com>
2020-06-26 20:43:19 +0300

    created Remote class and fields

--- app/src/main/java/com/dagger2/Model/Car.java
+++ app/src/main/java/com/dagger2/Model/Car.java
@@ -7,13 +7,16 @@ import javax.inject.Inject;
 public class Car {
     private static final String TAG = "Car";
 
-    private Engine engine;
+    @Inject Engine engine;
     private Wheels wheels;
 
     @Inject
-    public Car(Engine engine, Wheels wheels) {
-        this.engine = engine;
+    public Car(Wheels wheels) {
         this.wheels = wheels;
+    }
+    @Inject
+    public void enableRemote(Remote remote){
+        remote.setListener(this);
     }
     public void drive(){
         Log.d(TAG, "driving");
---
+++ app/src/main/java/com/dagger2/Model/Remote.java
@@ -0,0 +1,16 @@
+package com.dagger2.Model;
+
+import android.util.Log;
+
+import javax.inject.Inject;
+
+public class Remote {
+    private static final String TAG = "Remote";
+
+    @Inject
+    public Remote() {
+    }
+    public void setListener(Car car){
+        Log.d(TAG, "remote connected");
+    }
+}
